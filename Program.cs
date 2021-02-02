3649204cb8c89c11feca1cac488b610eee69af12
matijamaricdev <matijamaricdev@gmail.com>
2021-02-02 16:45:40 +0100

    Console application - evidention of drivers licence initial main

---
+++ Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOPEvidencijaVozackihDozvolaConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vozac> listaPostojecihVozaca = new List<Vozac>();
+
+            PonudiPonovnoIzbornik(listaPostojecihVozaca);
+        }
+
+        private static int PonudiPonovnoIzbornik(List<Vozac> listaPostojecihVozaca)
+        {
+            int broj;
+            List<string> izbornik = new List<string>() { "Dodavanje postojeceg vozaca", "Azuriranje postojeceg vozaca", "Ispis svih vozaca", "Ispis vozaca sa vazecom vozackom dozvolom",
+            "Ispis vozaca sa isteklom vozackom dozvolom", "Prekid rada programa"};
+            Console.WriteLine("Dobrodošli na izbornik evidencije vozačkih dozvola. Odaberite što želite (broj označava broj i stavku koju ste odabrali");
+            for (int i = 0; i < izbornik.Count; i++)
+            {
+                Console.WriteLine("{0} - {1}", i, izbornik[i]);
+            }
+            broj = Convert.ToInt32(Console.ReadLine());
+
+            switch (broj)
+            {
+                case 0:
+                    DodajNovogVozaca(listaPostojecihVozaca);
+                    break;
+                case 1:
+                    AzuriranjePostojecegVozaca(listaPostojecihVozaca);
+                    break;
+                case 2:
+                    IspisSvihVozaca(listaPostojecihVozaca);
+                    break;
+                case 3:
+                    IspisSvihVozacaSaVazecomVozackomDozvolom(listaPostojecihVozaca);
+                    break;
+                case 4:
+                    IspisSvihVozacaSaIsteklomVozackomDozvolom(listaPostojecihVozaca);
+                    break;
+                case 5:
+                    Console.WriteLine("Prekidam rad programa");
+                    break;
+            }
+
+            return broj;
+        }
+
+        private static void DodajNovogVozaca(List<Vozac> listaPostojecihVozaca)
+        {
+            int broj;
+
+            Console.WriteLine("Upiši ime na vozackoj dozvoli");
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Upiši prezime na vozackoj dozvoli");
+            string prezime = Console.ReadLine();
+
+            Console.WriteLine("Upiši OIB osobe na vozackoj dozvoli");
+            double OIB = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Odaberite kategoriju (broj označava broj i stavku koju ste odabrali)");
+            Vozac vozac = new Vozac(ime, prezime, OIB, DateTime.Now.Date, DateTime.Now.Date.AddYears(5));
+
+            for (int i = 0; i < vozac.PopisKategorija.Count; i++)
+            {
+                Console.WriteLine("{0} - {1}", i, vozac.PopisKategorija[i]);
+            }
+            broj = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Odabrali ste kategoriju: {0}", vozac.PopisKategorija[broj]);
+            listaPostojecihVozaca.Add(vozac);
+            
+            foreach(var c in vozac.PopisKategorija)
+            {
+                Console.WriteLine(c);
+            }
+
+            PonudiPonovnoIzbornik(listaPostojecihVozaca);
+        }
+
+        private static void AzuriranjePostojecegVozaca(List<Vozac> listaPostojecihVozaca)
+        {
+            Vozac azurirajOvogVozaca;
+            Console.WriteLine("Dobrodošli na izbornik ažuriranja postojećih vozača. Napišite ime i prezime vozača kojeg želite promijeniti");
+            for (int i = 0; i < listaPostojecihVozaca.Count; i++)
+            {
+                Console.WriteLine("{0} {1}", listaPostojecihVozaca[i].Ime, listaPostojecihVozaca[i].Prezime);
+            }
+            string imeIPrezime = Console.ReadLine();
+
+            foreach(var c in listaPostojecihVozaca)
+            {
+                if (c.Ime == imeIPrezime.Split(' ')[0] && c.Prezime == imeIPrezime.Split(' ')[1])
+                {
+                    Console.WriteLine("Odabrali ste ažurirati vozača imena {0} i prezimena {1}", c.Ime, c.Prezime);
+                    azurirajOvogVozaca = new Vozac(c.Ime, c.Prezime, c.OIB, c.DatumIzdavanjaVozacke, c.DatumIstekaVozacke);
+                    listaPostojecihVozaca.Remove(c);
+                }
+            }
+
+            Console.WriteLine("Upiši novo ime na vozackoj dozvoli");
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Upiši novo prezime na vozackoj dozvoli");
+            string prezime = Console.ReadLine();
+
+            Console.WriteLine("Upiši novi OIB osobe na vozackoj dozvoli");
+            double OIB = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Odaberite novu kategoriju (broj označava broj i stavku koju ste odabrali)");
+            Vozac vozac = new Vozac(ime, prezime, OIB, DateTime.Now.Date, DateTime.Now.Date.AddYears(5));
+
+            for (int i = 0; i < vozac.PopisKategorija.Count; i++)
+            {
+                Console.WriteLine("{0} - {1}", i, vozac.PopisKategorija[i]);
+            }
+            listaPostojecihVozaca.Add(vozac);
+            PonudiPonovnoIzbornik(listaPostojecihVozaca);
+        }
+
+        private static void IspisSvihVozaca(List<Vozac> listaPostojecihVozaca)
+        {
+            for(int i = 0; i < listaPostojecihVozaca.Count; i++)
+            {
+                Console.WriteLine(listaPostojecihVozaca[i].Ime + " " + listaPostojecihVozaca[i].Prezime);
+            }
+            PonudiPonovnoIzbornik(listaPostojecihVozaca);
+        }
+
+        private static void IspisSvihVozacaSaVazecomVozackomDozvolom(List<Vozac> listaPostojecihVozaca)
+        {
+            for(int i = 0; i < listaPostojecihVozaca.Count; i++)
+            {
+                if (listaPostojecihVozaca[i].DatumIstekaVozacke > DateTime.Now.Date)
+                {
+                    Console.WriteLine(listaPostojecihVozaca[i].Ime + " " + listaPostojecihVozaca[i].Prezime);
+                }
+            }
+            PonudiPonovnoIzbornik(listaPostojecihVozaca);
+        }
+
+        private static void IspisSvihVozacaSaIsteklomVozackomDozvolom(List<Vozac> listaPostojecihVozaca)
+        {
+            for (int i = 0; i < listaPostojecihVozaca.Count; i++)
+            {
+                if (listaPostojecihVozaca[i].DatumIstekaVozacke < DateTime.Now.Date)
+                {
+                    Console.WriteLine(listaPostojecihVozaca[i].Ime + " " + listaPostojecihVozaca[i].Prezime);
+                }
+            }
+            PonudiPonovnoIzbornik(listaPostojecihVozaca);
+        }
+    }
+}
---
+++ Vozac.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPEvidencijaVozackihDozvolaConsole
+{
+//    Naslov zadataka: Evidencija vozačkih dozvola
+//Opis zadatka: 
+//Kreirajte klasu imena "Vozač" koja sadrži šest svojstava(varijabli):
+//Ime
+//Prezime
+//OIB
+//Datum izdavanja vozačke dozvole
+//Datum isteka vozačke dozvole
+//Popis kategorija vozila za koje vrijedi vozačka dozvola(složena struktura podataka)
+//Kreirajte konstruktor koji prima pet parametara(svim parametri osim popisa kategorije vozila za koje vrijedi vozačka dozvola).
+//Kreirajte get i set metode za sve parametre.Set metoda za popis kategorija vozila mora omogućiti dodavanje nove kategorije vozila u složenu strukturu podataka (u složenoj strukturi podataka ne smije biti duplikati) i brisanje jedne od kategorija vozila(preporuka: napraviti dvije set metode).
+//Kreirajte C# program koji sadrži izbornik sa sljedećim opcijama:
+//Dodavanje novog vozača
+//Ažuriranje postojećeg vozača
+//Ispis svih vozača
+//Ispis vozača sa važećom vozačkom dozvolom
+//Ispis vozača sa isteklom vozačkom dozvolom
+//Prekid rada programa
+//Podaci svih vozača moraju biti pohranjeni u listi, tj. svaki novi vozač(objekt) mora biti dodan u listu vozača.
+//Opcija ažuriranja podataka postojećeg vozača mora omogućiti izmjenu podataka vozača iz liste.
+//Nakon odabira određene opcije i izvođenja potrebnih operacija vezanih za odabranu opciju, program mora ponovo ponuditi izbornik i omogućiti korisniku novi izbor.
+//Program se mora izvoditi sve dok na izborniku nije odabrana opcija prekida rada programa.
+    public class Vozac
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public double OIB { get; set; }
+        public DateTime DatumIzdavanjaVozacke { get; set; }
+        public DateTime DatumIstekaVozacke { get; set; }
+        public List<string> PopisKategorija { get; set; } = new List<string>() { "A", "B", "C", "D", "E", "F" }; 
+        //shorthand set metoda, lako se doda nova kategorija sa PopisKategorija.Add("G") ili PopisKategorija.Remove("G")
+
+        public Vozac(string ime, string prezime, double oib, DateTime datumIzdavanjaVozacke, DateTime datumIstekaVozacke)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            OIB = oib;
+            DatumIzdavanjaVozacke = datumIzdavanjaVozacke;
+            DatumIstekaVozacke = datumIstekaVozacke;
+        }
+    }
+}
